055587f10ba3512fca69fc1aa851de73d03dd404
dieatmore <s.d.wang.1@nefu.edu.cn>
2026-04-25 23:21:05 +0800

    init

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 
 
-app = FastAPI(title="FastAPI Learn")
+app = FastAPI()
 
+# 路由分发 include_router
+# app.include_router(shop, prefix="/shop", tags=["购物中心接口"])
 
 @app.get("/")
-def read_root() -> dict[str, str]:
+async def read_root():
     return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/test", tags=["test"], description="This is a test 接口", summary="测试接口")
+async def test():
+    return {"message": "一个测试接口"}
